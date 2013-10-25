9454ce5e3f313d54744cbf2f0293d55f1168afbc
Benjamin Muschko <benjamin.muschko@gmail.com>
2013-10-25 16:51:57 -0400

    Added library project to demonstrate creation and consumption of AAR

---
+++ library-project/app/src/main/java/org/gradle/examples/android/app/MainActivity.java
@@ -0,0 +1,18 @@
+package org.gradle.examples.android.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.gradle.examples.android.lib.Utils;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+        TextView text = new TextView(this);
+        text.setText("Hello World of " + Utils.capitalize("android") + "!");
+        setContentView(text);
+	}
+}
---
+++ library-project/lib/src/main/java/org/gradle/examples/android/lib/Utils.java
@@ -0,0 +1,9 @@
+package org.gradle.examples.android.lib;
+
+import org.apache.commons.lang.StringUtils;
+
+public class Utils {
+    public static String capitalize(String s) {
+        return StringUtils.capitalize(s);
+    }
+}
